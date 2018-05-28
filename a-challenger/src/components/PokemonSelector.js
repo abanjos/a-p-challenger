67109900ceda9dad04189197d25dd110210450ed
aanjos <andrebrandg@gmail.com>
2018-05-28 14:38:04 -0300

    Remove semicolon

--- a-challenger/src/components/PokemonSelector.js
+++ a-challenger/src/components/PokemonSelector.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import PokemonButton from './PokemonButton';
+import PokemonButton from './PokemonButton'
 
-const api = 'https://pokeapi.co/api/v2';
+const api = 'https://pokeapi.co/api/v2'
 
 class PokemonSelector extends Component {
   constructor(props){
-    super(props);
+    super(props)
 
     this.state = {
       actual: null,
@@ -17,33 +17,33 @@ class PokemonSelector extends Component {
   } 
   
   componentDidMount() {
-    const url = this.state.actual == null ? `${api}/pokemon` : this.state.actual;
+    const url = this.state.actual == null ? `${api}/pokemon` : this.state.actual
     fetch(url)
       .then(response => response.json())
-      .then(data => this.setState({ actual: url, previous: data.previous, next: data.next, list: data.results }));
+      .then(data => this.setState({ actual: url, previous: data.previous, next: data.next, list: data.results }))
   }
 
   async getPokemonsList() {
     return fetch(`${api}pokemon`)
-      .then(response => response.json());
+      .then(response => response.json())
   }
 
   createPokemonButtons() {
     return this.state.list.map(pokemon =>
       <PokemonButton key={pokemon.name} url={pokemon.url} /> 
-    );
+    )
   }
 
   render() {
 
-    let pokemonButtons = this.createPokemonButtons();
+    let pokemonButtons = this.createPokemonButtons()
 
     return (
       <div className="Pokemon-Selector">
         {pokemonButtons}
       </div>
-    );
+    )
   }
 }
 
-export default PokemonSelector;
+export default PokemonSelector
